55a6fc9bed0a610fa3c11613a4fd62e7ab78c90f
daeunleeeee <lde7953@gmail.com>
2024-01-31 14:24:06 +0900

    [FEAT] create button component

---
+++ mydevposition/src/components/Button.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  display: flex;
+  margin: 3rem auto 3rem auto;
+
+  width: 15rem;
+  height: 4rem;
+  justify-content: center;
+  text-align: center;
+
+  border-radius: 45px;
+  border: 3px solid #f7941e;
+  background: #ffe4c3;
+`;
+
+export default Button;
