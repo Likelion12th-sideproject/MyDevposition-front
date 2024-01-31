4a3e3eda39da85479c1551bf2abc39694a87eb2f
daeunleeeee <lde7953@gmail.com>
2024-01-31 14:25:50 +0900

    [FEAT] create result-page

---
+++ mydevposition/src/pages/Result.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Wrapper from "../components/Wrapper";
+
+const Result = () => {
+  return (
+    <Wrapper>
+      <div>
+        <h1>개발 포지션 테스트 결과</h1>
+        <p>결과 표시 </p>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Result;
