66f3fdec58b3a9ab348a90db4a9a045db3942864
daeunleeeee <lde7953@gmail.com>
2024-01-31 14:24:42 +0900

    [FEAT] create questioncard component (outer border)

---
+++ mydevposition/src/components/QuestionCard.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  padding-top: 10rem;
+  margin: auto;
+  text-align: center;
+  width: 40rem;
+  height: 45rem;
+`;
+
+export default Card;
